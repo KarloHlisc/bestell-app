a6487f00788b45772b68411aba640cfa894d3820
Karlo Hlisc <karlohlisc.kh@gmail.com>
2025-10-02 09:42:10 +0200

    add projekt

---
+++ scripts/db.js
@@ -0,0 +1,56 @@
+let myDishes = [
+    {
+        "name":"Spagetti Carbonara",
+        "price":12.99,
+        "description":"Cremige Psta mit Spek und Parmesan, verfeinert mit Bro"
+    },
+     {
+        "name":"Vegetarische Spagetti",
+        "price":2.99,
+        "description":"Cremige Psta mit Spek und Parmesan, verfeinert mit Bro"
+    },
+     {
+        "name":"Gegrilltes Heanchen mit Quinoa",
+        "price":14.99,
+        "description":"Cremige Psta mit Spek und Parmesan, verfeinert mit Bro"
+    },
+     {
+        "name":"Thai Curry mit Garnelen",
+        "price":12.99,
+        "description":"Cremige Psta mit Spek und Parmesan, verfeinert mit Bro"
+    },
+     {
+        "name":"Spagetti Carbonara",
+        "price":13.75,
+        "description":"Cremige Psta mit Spek und Parmesan, verfeinert mit Bro"
+    },
+     {
+        "name":"Spagetti Bolognese",
+        "price":11.53,
+        "description":"Cremige Pasta mit Rind- und Schweinefleisch, verfeinert mit Bro"
+    },
+]
+
+
+let myDrinks = [
+    {
+        "name":"Coca-cola",
+        "price":3.75,
+        "description":"Gasiertes erfrischrender Getrank. Entaltet Kolensuere und Zuker"
+    },
+    {
+        "name":"Bier Helles",
+        "price":5.75,
+        "description":"Erfrischendes Helles Bier"
+    },
+    {
+        "name":"Kraftbier",
+        "price":13.75,
+        "description":"Starkes erfrischender Bier. Enthaltet mehr Alkohol"
+    },
+    {
+        "name":"Wasser",
+        "price":3.75,
+        "description":"Der einziger Durstloescher"
+    },
+]
---
+++ scripts/template.js
@@ -0,0 +1,29 @@
+function displayFoodBox(){
+    let cardFoodRef = document.getElementById("food-box-display");
+    cardFoodRef.innerHTML="";
+    for(let index = 0 ; index < myDishes.length; index++){
+        cardFoodRef.innerHTML+=`<div class="food-content">
+        <div class="food-name-section">
+                <h3 id="food-name-${index}">${myDishes[index].name}</h3>
+                <button id="btn-add-food-${index}" class="btn-add-food">+</button>
+            </div>
+            <p class="food-description" id="food-description">${myDishes[index].description}</p>
+            <p class="food-price" id="food-price">${myDishes[index].price} €</p>
+        </div>`;
+    }
+}
+
+function displayDrinkBox(){
+    let cardFoodRef = document.getElementById("drink-box-display");
+    cardFoodRef.innerHTML="";
+    for(let index = 0 ; index < myDrinks.length; index++){
+        cardFoodRef.innerHTML+=`<div class="drink-content">
+        <div class="food-name-section">
+                <h3 id="food-name-${index}">${myDrinks[index].name}</h3>
+                <button id="btn-add-food-${index}" class="btn-add-food">+</button>
+            </div>
+            <p class="food-description" id="food-description">${myDrinks[index].description}</p>
+            <p class="food-price" id="food-price">${myDrinks[index].price} €</p>
+        </div>`;
+    }
+}
