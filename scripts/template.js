585108e4bfae3e0c7cf54809305c8392f27cd2e3
Karlo Hlisc <karlohlisc.kh@gmail.com>
2025-10-17 17:48:25 +0200

    changes to the object

--- scripts/template.js
+++ scripts/template.js
@@ -2,13 +2,14 @@ function displayFoodBox(){
     let cardFoodRef = document.getElementById("food-box-display");
     cardFoodRef.innerHTML="";
     for(let index = 0 ; index < myDishes.length; index++){
-        cardFoodRef.innerHTML+=`<div class="food-content">
+        let singleDish = myDishes[index];
+        cardFoodRef.innerHTML+=`<div class="food-content" id="${singleDish.id}">
         <div class="food-name-section">
-                <h3 id="food-name-${index}">${myDishes[index].name}</h3>
+                <h3 id="food-name-${index}">${singleDish.name}</h3>
                 <button id="btn-add-food-${index}" class="btn-add-food" onclick="addNote(${index})">+</button>
             </div>
-            <p class="food-description" id="food-description">${myDishes[index].description}</p>
-            <p class="food-price" id="food-price">${myDishes[index].price} €</p>
+            <p class="food-description" id="food-description">${singleDish.description}</p>
+            <p class="food-price" id="food-price">${singleDish.price} €</p>
         </div>`;
     }
 }
@@ -17,13 +18,15 @@ function displayDrinkBox(){
     let cardFoodRef = document.getElementById("drink-box-display");
     cardFoodRef.innerHTML="";
     for(let index = 0 ; index < myDrinks.length; index++){
+        let singleDrink = myDrinks[index];
         cardFoodRef.innerHTML+=`<div class="drink-content">
         <div class="food-name-section">
-                <h3 id="food-name-${index}">${myDrinks[index].name}</h3>
+                <h3 id="food-name-${index}">${singleDrink.name}</h3>
                 <button id="btn-add-food-${index}" class="btn-add-food" onclick="addNote(${index})">+</button>
             </div>
-            <p class="food-description" id="food-description">${myDrinks[index].description}</p>
-            <p class="food-price" id="food-price-${index}">${myDrinks[index].price} €</p>
+            <p class="food-description" id="food-description">${singleDrink.description}</p>
+            <p class="food-price" id="food-price-${index}">${singleDrink.price} €</p>
         </div>`;
     }
 }
+
